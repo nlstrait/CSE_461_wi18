c9d9ddd95a8ce7f7f220a2bd2ea69cb2a5a99b96
Oliver Sheridan <westward@uw.edu>
2018-01-19 22:21:35 -0800

    stage B working for server

--- server.py
+++ server.py
@@ -11,19 +11,16 @@ def verify_header(message, psecret, step, sid):
     if len(message) % 4 != 0:
         return False
 
-    payload_len = unpack('!I', message[0:4])[0]
+    payload_len, secret, step_num, student_id = unpack('!IIHH', message[0:12])
     if len(message) - 12 < payload_len:
         return False
 
-    secret = unpack('!I', message[4:8])[0]
     if secret != psecret:
         return False
 
-    step_num = unpack('!H', message[8:10])[0]
     if step_num != step:
         return False
 
-    student_id = unpack('!H', message[10:12])[0]
     if student_id != sid:
         return False
 
@@ -34,16 +31,13 @@ def verify_header(message, psecret, step, sid):
 
 # this extracts the payload length, secret, and step number, and returns them in that order
 def parse_header(message):
-    return unpack('!I', message[0:4])[0], unpack('!I', message[4:8])[0], unpack('!H', message[8:10])[0]
+    return unpack('!IIH', message[0:10])
 
 
 # helper function to generate a new packet header
 def generate_header(payload_len, secret, step_num, sid):
     header = bytearray()
-    header.extend(pack('!I', payload_len))
-    header.extend(pack('!I', secret))
-    header.extend(pack('!H', step_num))
-    header.extend(pack('!H', sid))
+    header.extend(pack('!IIHH', payload_len, secret, step_num, sid))
     return header
 
 
@@ -74,23 +68,66 @@ def handle_thread(sock, message, address):
     num = random.randint(1,20)
     length = random.randint(1,20)
     udp_port = random.randint(49152, 65535) # stick to the dynamic ports
-    secret_a = random.randint(1,100)
+    secret_a = random.randint(1,1000)
 
-    new_message = generate_header(16, 0, 2, 361)
-    new_message.extend(pack('!I', num))
-    new_message.extend(pack('!I', length))
-    new_message.extend(pack('!I', udp_port))
-    new_message.extend(pack('!I', secret_a))
+    outgoing_packet = generate_header(16, 0, 2, 361)
+    outgoing_packet.extend(pack('!IIII', num, length, udp_port, secret_a))
 
-    sock.sendto(new_message, address)
+    sock.sendto(outgoing_packet, address)
+
 
     '''
     ===============STAGE B================
     '''
+    stage_b_socket = socket(AF_INET, SOCK_DGRAM)
+    stage_b_socket.bind(('', udp_port))
 
-    new_socket = socket(AF_INET, SOCK_DGRAM)
-    new_socket.bind(('', udp_port))
+    num_acked = 0 # the number of packets acked so far
+    while num_acked != num:
+        print 'loop'
+        packet_b, addr_b = stage_b_socket.recvfrom(4096)
 
+        # drop if packet is malformed
+        if not verify_header(packet_b, secret_a, 1, 361):
+            return
+
+        payload_len, secret, step_num = parse_header(packet_b)
+        if payload_len != length + 4:
+            return
+
+
+        payload = packet_b[12:]
+        packet_id = unpack('!I', payload[0:4])[0]
+        if packet_id != num_acked: # packets must be received in order; wait for the next packet
+            continue
+
+
+
+        for byte in payload[4:length]:
+            if byte != '\0': # packet payload must be all zero bytes; if not, drop connection
+                return
+
+        # all's well; now decide whether to skip the ack step
+        if random.randint(0,1):
+            continue
+
+        outgoing_packet = generate_header(4, secret_a, 1, 361)
+        outgoing_packet.extend(pack('!I', num_acked))
+        stage_b_socket.sendto(outgoing_packet, addr_b)
+        num_acked += 1
+
+    print 'past loop'
+    # got all the packets, now for step b2
+    tcp_port = random.randint(49152, 65535) # stick to the dynamic ports
+    secret_b = random.randint(1,1000)
+
+    outgoing_packet = generate_header(8, secret_a, 2, 361)
+    outgoing_packet.extend(pack('!II', tcp_port, secret_b))
+    stage_b_socket.sendto(outgoing_packet, address)
+
+    '''
+    ===============STAGE C================
+    '''
 
     return
 
