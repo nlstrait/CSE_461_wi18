1a9dbb405062d559d5650f8d754c4508711924af
None <None>
2018-02-11 14:59:53 -0800

    added part4 files

--- project2/part4/part4.py
+++ project2/part4/part4.py
@@ -12,16 +12,16 @@ class part3_topo(Topo):
   def build(self):
     #note the default route addition
     #h10 = self.addHost('h10',mac='00:00:00:00:00:01',ip='10.0.1.10/24',defaultRoute='h10-eth0')
-    h10 = self.addHost('h10', mac='00:00:00:00:00:01', ip='10.0.1.10/24', defaultRoute='h10-eth0')
-    h20 = self.addHost('h20', mac='00:00:00:00:00:02', ip='10.0.2.20/24', defaultRoute='h10-eth0')
-    h30 = self.addHost('h30', mac='00:00:00:00:00:03', ip='10.0.3.30/24', defaultRoute='h10-eth0')
+    h10 = self.addHost('h10', mac='00:00:00:00:00:01', ip='10.0.1.10/24', defaultRoute='via 10.0.1.1')
+    h20 = self.addHost('h20', mac='00:00:00:00:00:02', ip='10.0.2.20/24', defaultRoute='via 10.0.1.1')
+    h30 = self.addHost('h30', mac='00:00:00:00:00:03', ip='10.0.3.30/24', defaultRoute='via 10.0.1.1')
     s1 = self.addSwitch('s1')
     s2 = self.addSwitch('s2')
     s3 = self.addSwitch('s3')
     cores21 = self.addSwitch('cores21')
     dcs31 = self.addSwitch('dcs31')
-    serv1 = self.addHost('serv1', mac='00:00:00:00:00:04', ip='10.0.4.10/24', defaultRoute='h10-eth0')
-    hnotrust = self.addHost('hnotrust', mac='00:00:00:00:00:05', ip='172.16.10.100/24', defaultRoute='h10-eth0')
+    serv1 = self.addHost('serv1', mac='00:00:00:00:00:04', ip='10.0.4.10/24', defaultRoute='via 10.0.1.1')
+    hnotrust = self.addHost('hnotrust', mac='00:00:00:00:00:05', ip='172.16.10.100/24', defaultRoute='via 10.0.1.1')
 
     self.addLink(h10, s1)
     self.addLink(h20, s2)
