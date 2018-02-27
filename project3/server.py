320746904a1ad8fb39f5e80a43cd944f76c06e69
Oliver Sheridan <westward@uw.edu>
2018-02-26 21:47:27 -0800

    starting to parse HTTP headers

--- project3/server.py
+++ project3/server.py
@@ -15,19 +15,56 @@ def receive_from_stream(sock, num_bytes):
 
     return received
 
+def parse_http_request(header):
+    lines = header.split('\n')
+
+    # remove witespace at ends
+    for i in range(len(lines)):
+        lines[i] = lines[i].strip()
+
+    # these will get set in a bit
+    URI_and_proto = ""
+    host = ""
+
+    for line in lines:
+        if line[:3] == 'GET':
+            URI_and_proto = line[3:].strip()
+        if line[:7] == 'CONNECT':
+            URI_and_proto = line[7:].strip()
+        if line[:5].lower() == 'host:':
+            host = line[5:].strip()
+
+    print URI_and_proto
+    print host
+
 # this handles a single client
-def handle_thread(sock, message, address):
-    # handle new client
+def handle_thread(sock, address):
+    input_buffer = bytearray()
+    while len(input_buffer) < 4 or input_buffer[-4:] != b'\r\n\r\n':
+        input_buffer.extend(receive_from_stream(sock, 1)) # i know pulling one byte at a time is slow; it works for now though
+
+    # we now have a complete HTTP request header
+
+
+    parse_http_request(input_buffer.decode("utf-8"))
+    #print len(input_buffer), input_buffer.decode("utf-8")
+
+
+    sock.close()
+
 
 def main():
-    sock = socket(AF_INET, SOCK_DGRAM)
+    sock = socket(AF_INET, SOCK_STREAM)
     port = 12235
     sock.bind(('', port))
+    sock.listen(10) # queue up to 10 requests
+
+    print "waiting for connections..."
 
     while True:
-        message, address = sock.recvfrom(4096)
-        print 'Got connection from', address
-        thread.start_new_thread(handle_thread, (sock, message, address))
+        client_sock, client_addr = sock.accept()
+        print 'Got connection from', client_addr
+        thread.start_new_thread(handle_thread, (client_sock, client_addr))
 
 
 
---
+++ project3/test_client.py
@@ -0,0 +1,69 @@
+from socket import *
+import random
+import struct
+from  struct import pack, unpack
+import sys
+
+#IP = "attu2.cs.washington.edu"
+IP = "127.0.0.1"
+
+# this also verifies the length and padding of the packet
+def verify_header(message, psecret, step, sid):
+    if len(message) < 12:
+        return False
+
+    if len(message) % 4 != 0:
+        return False
+
+    payload_len, secret, step_num, student_id = unpack('!IIHH', message[0:12])
+    if len(message) - 12 < payload_len:
+        return False
+
+    if secret != psecret:
+        return False
+
+    if step_num != step:
+        return False
+
+    if student_id != sid:
+        return False
+
+    print 'header verified'
+
+    return True
+
+
+# this extracts the payload length, secret, and step number, and returns them in that order
+def parse_header(message):
+    return unpack('!IIH', message[0:10])
+
+
+# helper function to generate a new packet header
+def generate_header(payload_len, secret, step_num, sid):
+    header = bytearray()
+    header.extend(pack('!IIHH', payload_len, secret, step_num, sid))
+    return header
+
+
+# reliably pulls a given number of bytes from a stream socket
+def receive_from_stream(sock, num_bytes):
+    buf = bytearray()
+    received = bytearray()
+
+    while len(received) < num_bytes:
+        buf = sock.recv(num_bytes - len(received))
+        received.extend(buf)
+
+    return received
+
+def main():
+
+    sock = socket(AF_INET, SOCK_STREAM)
+    sock.connect(('127.0.0.1', 12235))
+
+    sock.send(b'GET http://www.washington.edu/ HTTP/1.0\r\n\r\n')
+    
+
+
+if __name__ == '__main__':
+    main()
